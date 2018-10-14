a82e7612ad16bff364ad14882ed23e34d475bcfd
carrloseduardograjalescano <44042460+carrloseduardograjalescano@users.noreply.github.com>
2018-10-14 16:29:07 -0500

    hecho por carlos grajales

---
+++ Documents/NetBeansProjects/pregunta4/src/pregunta4/BeffredReader.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pregunta4;
+
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author Carlos Eduardo
+ */
+public class BeffredReader {
+
+    BeffredReader(InputStreamReader inputStreamReader) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    BeffredReader(InputStreamReader inputStreamReader) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    BeffredReader(InputStreamReader inputStreamReader) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    BeffredReader(InputStreamReader inputStreamReader) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    BeffredReader(InputStreamReader inputStreamReader) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ Documents/NetBeansProjects/pregunta4/src/pregunta4/Pregunta4.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pregunta4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author Carlos Eduardo
+ */
+public class Pregunta4 {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+       
